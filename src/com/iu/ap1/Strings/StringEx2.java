90e8b27f8281dc6cd13944951e270476537b513f
Parkyechan <himan2623@naver.com>
2023-01-06 10:12:10 +0900

    1

--- src/com/iu/ap1/Strings/StringEx2.java
+++ src/com/iu/ap1/Strings/StringEx2.java
@@ -7,14 +7,17 @@ public class StringEx2 {
 	public static void main(String[] args) {
 		
 		Scanner sc =new Scanner(System.in);
-		String [ ] names = {"jpg","jpeg","gif","png"};
+		String names = {"jpg","jpeg","gif","png"};
 		System.out.println(" 파일명 입력");
 		String fileName = sc.next();//sf.sd.dsf.df
+			
+		for(int i =0 ;i<names.length ;i++ ) {
+			
+			names = fileName.substring(1);
+			
+		}
 		
-		
-		
-		
-		
+	    
 		
 		
 		
