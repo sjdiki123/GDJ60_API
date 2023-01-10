aed3c3b949f1b28718ce7b1c9de75e1b0428a22d
Parkyechan <himan2623@naver.com>
2023-01-10 14:31:03 +0900

    1

--- src/com/iu/api2/collections/ex1/Ex1Main.java
+++ src/com/iu/api2/collections/ex1/Ex1Main.java
@@ -1,10 +1,43 @@
 package com.iu.api2.collections.ex1;
 
+import java.util.ArrayList;
+
 public class Ex1Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 학생은 이름 번호 국어 영어 수학 총점으로 평균으로 이루어짐
+		//1. 학생정보 초기화
+		//2 학생 정보조회
+		//3.학생 정보 검새 조화(이름
+		//4.학생 정보 추가 
+		//5학생 정보 삭제 (이름
+		//6종료
+		
+		
+		
+		
+		StudentDAO dao = new StudentDAO();
+   ArrayList<StudentDTO> ar = dao.init();
+   
+   StudentDTO studentDTO = dao.findByName(ar);
+//   System.out.println(studentDTO.getName());
+//   System.out.println(studentDTO.getAvg());
+   
+   dao.addStudent(ar);
+   
+   
+for(int i =0;i<ar.size();i++) {
+	System.out.println(ar.get(i).getName());
+}
+	   
+   }
+   //향샹된 for
+//		for(StudentDTO studentDTO:ar) {
+//		}
+//		int[]nums = new int [3];
+//		for(int n:nums) {
+//			
+//		}
 	}
 
-}
+
--- src/com/iu/api2/collections/ex1/StudentDAO.java
+++ src/com/iu/api2/collections/ex1/StudentDAO.java
@@ -1,5 +1,90 @@
 package com.iu.api2.collections.ex1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class StudentDAO {
 
+	
+	private StringBuffer sb;
+	private Scanner sc;
+	
+	public StudentDAO() {
+		this.sc=new Scanner(System.in); 
+		this.sb = new StringBuffer();
+		
+		
+		sb.append("iu-1-90-60-70-");
+		sb.append("winter-2-86-84-75-");
+		sb.append("suji, 3 ,89, 74 ,87");
+		sb.append("choa, 4, 71, 25, 99");
+		
+		    
+	}
+	
+	// 학생 정보 추가
+    public void addStudent(ArrayList<StudentDTO> ar) {
+    	StudentDTO studentDTO = new StudentDTO();
+    	System.out.println("이름을 입력");
+    	studentDTO.setName(sc.next());
+    	System.out.println("번호를 입력");
+    	studentDTO.setNum(sc.nextInt());
+    	System.out.println("국어점수를 입력");
+    	studentDTO.setKor(sc.nextInt());
+    	System.out.println("수학점수를 입력");
+    	studentDTO.setMath(sc.nextInt());
+    	System.out.println("영어점수를 입력");
+    	studentDTO.setEng(sc.nextInt());
+    	studentDTO.setTotal(studentDTO.getKor()+studentDTO.getMath()+studentDTO.getEng());
+		studentDTO.setAvg(studentDTO.getTotal()/3.0);
+		ar.add(studentDTO);
+    	
+    	
+    }
+	
+	
+	
+	
+	//학생 정보 검색
+	public StudentDTO findByName(ArrayList<StudentDTO> ar) {
+		System.out.println("검색할 이름 입력");
+		String name = sc.next();
+
+		StudentDTO st =null;
+		
+	for(StudentDTO studentDTO :ar) {
+		if(name.equals(studentDTO.getName())) {
+			st =studentDTO;
+			break;
+		}
+	}
+	return st;
+
+	}
+
+	
+	//학생정보 초기화
+	public ArrayList<StudentDTO> init() {
+		String data = this.sb.toString();
+		data= data.replace(" " , "-");
+		data= data.replace("," , "");	
+		
+		 System.out.println(data);
+      StringTokenizer st = new StringTokenizer(data, "-");
+      ArrayList<StudentDTO> ar = new ArrayList<>();
+		while (st.hasMoreTokens()) {
+			StudentDTO studentDTO = new StudentDTO();
+			studentDTO.setName(st.nextToken());
+			studentDTO.setNum(Integer.parseInt(st.nextToken()));
+			studentDTO.setKor(Integer.parseInt(st.nextToken()));
+			studentDTO.setEng(Integer.parseInt(st.nextToken()));
+			studentDTO.setMath(Integer.parseInt(st.nextToken()));
+			studentDTO.setTotal(studentDTO.getKor()+studentDTO.getMath()+studentDTO.getEng());
+			studentDTO.setAvg(studentDTO.getTotal()/3.0);
+			ar.add(studentDTO);
+		}
+		
+		return ar;
+	}
 }
